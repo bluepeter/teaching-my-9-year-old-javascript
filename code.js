685058bfa62d3eb30e3af8d52596eaa35b56d9e5
bluepeter <petertheyeti@gmail.com>
2020-04-20 13:32:10 -0700

    big numbers

--- code.js
+++ code.js
@@ -2,9 +2,9 @@
   document.open();
   document.write("<style>h1 {color: moonlightblue;}</style>");
   let peterJobs = [["Mcdonalds","stinky"], ["fluxguard","great"], ["cybersight","cool"]];
-  for (let i = 0; peterJobs.length > i; i++) { 
-document.write("<h1>");
-    document.write(peterJobs[i][0] + " is " + peterJobs[i][1]); 
-    document.write("<p>");
+  // for (let i = 0; peterJobs.length > i; i++) { 
+  for (let i = 1; 100 >= i; i++) {
+    //document.write(peterJobs[i][0] + " is " + peterJobs[i][1]); 
+    document.write("<span style='font-size: " + i * 10 + "px'>" + i + "</span><p>");
   }
 }
