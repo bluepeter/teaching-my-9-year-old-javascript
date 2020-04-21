9afb747b0a1e9dc8154f2947ac217010e1e3fdcd
bluepeter <petertheyeti@gmail.com>
2020-04-21 12:28:26 -0700

    Working w/ functions

--- code.js
+++ code.js
@@ -1,10 +1,14 @@
-{
-  document.open();
+document.open();
+function k(adj) {
   document.write("<style>h1 {color: moonlightblue;}</style>");
   let peterJobs = [["Mcdonalds","stinky"], ["fluxguard","great"], ["cybersight","cool"]];
-  // for (let i = 0; peterJobs.length > i; i++) { 
-  for (let i = 1; 100 >= i; i++) {
-    //document.write(peterJobs[i][0] + " is " + peterJobs[i][1]); 
-    document.write("<span style='font-size: " + i * 10 + "px'>" + i + "</span><p>");
+  for (let i = 0; peterJobs.length > i; i++) { 
+    document.write("<h1>");
+    document.write(peterJobs[i][0] + " is " + adj); 
+    document.write("</h1>");
   }
 }
+
+k("stinky");
+k("coooooooooooooooooooooooooooooooooollllllllll");
+k("Greeeeeeeeeaat");
