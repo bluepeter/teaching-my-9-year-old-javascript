0278565be09b8528c59a228e610f066022cc6495
bluepeter <petertheyeti@gmail.com>
2020-04-23 13:41:42 -0700

    Create random-writing.js

---
+++ random-writing.js
@@ -0,0 +1,8 @@
+document.open();
+let letters = ["a", "b", "c", "d", "e", "f", "g", " howdydoo ", "i", "j", "k", " ", " ", " ", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",  " ", " ", " ", " ", " ", " ", " ", " ", " ",];
+for( let i = 0;  i < 5000; i++) {
+  document.write(letters[Math.round(Math.random() * letters.length)]);
+  if(Math.random() * 1000 > 999) {
+    document.write("<p>");
+  }
+}
