947deffc2417623eb2b97a4257ad73954674344f
bluepeter <petertheyeti@gmail.com>
2020-05-07 13:43:15 -0700

    Create easy2.js

---
+++ easy2.js
@@ -0,0 +1,3 @@
+let people = [{name: "frank", age: 9678145, feetSize: 17}];
+document.open();
+document.write("hi my name is " + people[0].name + " I am " + people[0].age + " years old my feet size is " + people[0].feetSize);
