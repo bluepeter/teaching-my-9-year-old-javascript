97b61920363b3194557e73f5592139c31833851e
bluepeter <petertheyeti@gmail.com>
2020-05-09 14:19:05 -0700

    Create easy3.js

---
+++ easy3.js
@@ -0,0 +1,3 @@
+let people = [{name: "frank", age: Math.floor(Math.random() * 101), feetSize: 17}];
+document.open();
+document.write("hi my name is " + people[0].name + " I am " + people[0].age + " years old my feet size is " + people[0].feetSize);
