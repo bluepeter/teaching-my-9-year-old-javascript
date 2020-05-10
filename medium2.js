61e3769de22a1a5b99fd0b7a2731abf7b816c8fd
bluepeter <petertheyeti@gmail.com>
2020-05-10 15:35:03 -0700

    Create medium2.js

---
+++ medium2.js
@@ -0,0 +1,6 @@
+let people = [{name: "frank", age: Math.floor(Math.random() * 101), feetSize: 17}, {name: "icsnerf", age: 7392546719348, feetSize: 56}];
+document.open();
+for(const item in people) {
+  document.write("hi my name is " + item.name + " I am " + item.age + " years old my feet size is " + Math.round(Math.random() * 1009));
+  document.write("<br/>");
+}
