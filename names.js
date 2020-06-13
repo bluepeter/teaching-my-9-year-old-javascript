2534a516152e4c8c922213ed3b3790ffc88fd265
lightningFastHeater <66852904+lightningFastHeater@users.noreply.github.com>
2020-06-12 17:13:29 -0700

    Update names.js

--- names.js
+++ names.js
@@ -1,7 +1,22 @@
 document.open();
-let firstNames = ["odoles", "peter", "quil", "robot", "strange", "vasto", "willicer", "xino", "yoop", "zinloo"];
+let firstNames = [
+  "odoles",
+  "peter",
+  "quil",
+  "robot",
+  "strange",
+  "vasto",
+  "willicer",
+  "xino",
+  "yoop",
+  "zinloo",
+];
 let lastNames = ["ducku", "prince", "gray"];
-for( let i = 0;  i < 50; i++) {
-  document.write(firstNames[Math.floor(Math.random() * firstNames.length)] + " " + lastNames[Math.floor(Math.random() * lastNames.length)]);
+for (let i = 0; i < 50; i++) {
+  document.write(
+    firstNames[Math.floor(Math.random() * firstNames.length)] +
+      " " +
+      lastNames[Math.floor(Math.random() * lastNames.length)]
+  );
   document.write("<p>");
 }
