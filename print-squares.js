8c999a95d0efb08741a08d9f812da8bc96fcbf5f
lightningFastHeater <66852904+lightningFastHeater@users.noreply.github.com>
2020-06-14 11:39:15 -0700

    prettier

--- print-squares.js
+++ print-squares.js
@@ -1,9 +1,8 @@
 document.open();
 function s(num) {
- document.write( num * num );
+  document.write(num * num);
 }
-for( let i = 0;  i < 101; i++) {
+for (let i = 0; i < 101; i++) {
   s(i);
   document.write("<p>");
 }
-
