83ac10d7ad02eac249de3051202e9fa7bed4153d
lightningFastHeater <66852904+lightningFastHeater@users.noreply.github.com>
2020-06-16 23:16:55 -0700

    prettier

--- random-writing.js
+++ random-writing.js
@@ -1,8 +1,47 @@
 document.open();
-let letters = ["a", "b", "c", "d", "e", "f", "g", " howdydoo ", "i", "j", "k", " ", " ", " ", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z",  " ", " ", " ", " ", " ", " ", " ", " ", " ",];
-for( let i = 0;  i < 5000; i++) {
+let letters = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  " howdydoo ",
+  "i",
+  "j",
+  "k",
+  " ",
+  " ",
+  " ",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+  " ",
+];
+for (let i = 0; i < 5000; i++) {
   document.write(letters[Math.round(Math.random() * letters.length)]);
-  if(Math.random() * 1000 > 999) {
+  if (Math.random() * 1000 > 999) {
     document.write("<p>");
   }
 }
